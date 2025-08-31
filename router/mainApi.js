84965e153495217af1e40f57f44db72d7deaf12d
hosseini.milad1400 <milad.cia@gmail.com>
2025-08-31 12:08:27 +0330

    initial

--- router/mainApi.js
+++ router/mainApi.js
@@ -166,7 +166,7 @@ router.get('/get-product',auth, async (req,res)=>{
             if(result[i]){ 
                 var sku = result[i].ProductCode//?result[i].ProductCode:result[i].Code
             outPut.push(result[i])
-            var query = {title:result[i].SalesTitle,
+            var query = {title:result[i].Title,
                 sku:sku,
                 ItemID:result[i].Code,
                 unit:result[i].Unit,
